a1e6b6c273ed7109c99ce406ff0c68f85adebff5
soumyasics <soumya.sics@gmail.com>
2024-06-28 23:56:35 +0530

    movies

---
+++ Movies/movieSchema.js
@@ -0,0 +1,77 @@
+const mongoose= require("mongoose");
+
+const movieSchema=mongoose.Schema({
+   name:{
+        type:String,
+       
+        required:true,
+       
+    },
+    
+    genre:{
+        type:String,
+        
+        required:true,
+       
+    },
+    director:{
+        type:String,
+       
+        required:true,
+       
+       
+    },
+ 
+    scriptWriter:{
+        type:String,
+        required:true
+    },
+   
+    duration:{
+        type:String,
+        required:true
+
+    },
+    releaseDate:{
+        type:Date,
+        required:true
+
+    },
+    description:{
+        type:String,
+        required:true
+
+    },
+ 
+    language:{
+        type:String,
+        required:true
+
+    },
+    thumbnail:{
+        type:Object,
+        required:true
+
+    },
+    isActive:{
+        type:Boolean,
+        default:false
+    },
+    video:{
+        type:Object,
+        required:true
+        },
+        rating:{
+            type:Number,
+            default:0
+        },
+        adults:{
+            type:Boolean,
+        default:false
+        }
+  
+      
+});
+
+module.exports=mongoose.model('movies',movieSchema)
+
--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 const router=require('express').Router()
 const User=require('./User/userController')
+const movie=require('./Movies/movieController')
 
 
 //user routes
-//User routes
+
 router.post('/registerUser',User.upload, User.registerUser);
 router.post('/viewUserById/:id', User.viewUserById)
 router.post('/editUserById/:id',User.upload,User.editUserById);
@@ -15,5 +16,18 @@ router.post('/deleteUserById/:id', User.deleteUserById);
 router.post('/resetPassword/:id', User.resetPassword);
 router.post('/loginUser', User.login);
 router.post('/requireAuthUser', User.requireAuth);
+
+//movies
+
+router.post('/createMovie',movie.upload,movie.createMovie)
+router.post('/updateMovieById/:id',movie.upload,movie.updateMovieById)
+router.post('/deleteMovieById',movie.deleteMovieById)
+router.post('/addCast/:id',movie.uploadSingle,movie.addCast)
+router.post('/getMovieById/:id',movie.getMovieById)
+router.post('/approveMovieById/:id',movie.approveMovieById)
+router.post('/getMoviesByGenre/:genre',movie.getMoviesByGenre)
+router.post('/getMoviesByDirector/:director',movie.getMoviesByDirector)
+router.post('/getCastBYMovieId/:id',movie.getCastBYMovieId)
+router.post('/getMoviesByLanguage/:language',movie.getMoviesByLanguage)
 
 module.exports=router
